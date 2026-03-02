406e6cd4caf5ee6440bc066e2a3e6ba5ee2f4a53
ALENOR <ilyalepa333@gmail.com>
2026-03-02 19:25:41 +0300

    Translation of the text into English

--- Main.java
+++ Main.java
@@ -6,14 +6,14 @@ public class Main{
     public static void main(String args[]){
         String[] operators = {"+", "-", "*", "/"};
         try{
-            int choose = Choose(operators, "Выберите математический оператор:");
+            int choose = Choose(operators, "Select a mathematical operator: ");
             int[] numbers = InputNumbers();
             CalculatorClass calculator = new CalculatorClass(choose, numbers);
             int result = calculator.Calculate();
-            System.out.println("Ответ: " + result);
+            System.out.println("Answer: " + result);
         }
         catch (Exception e){
-            System.out.println("Возникла ошибка");
+            System.out.println("Error");
         }
     }
 
@@ -30,9 +30,9 @@ public class Main{
 
     public static int[] InputNumbers(){
 
-        System.out.println("Введите первое число или цифру");
+        System.out.println("Enter the first number or digit: ");
         int numberFirst = scan.nextInt();
-        System.out.println("Введите второе число или цифру");
+        System.out.println("Enter the second number or digit: ");
         int numberSecond = scan.nextInt();
         int[] resultCollection = {numberFirst, numberSecond};
         return resultCollection;
